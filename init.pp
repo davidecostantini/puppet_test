edbc2e6717a987caad3143e3208cb53e39238bc5
Davide <davide.costantini@infomentum.co.uk>
2015-07-17 15:34:56 +0100

    jj

--- init.pp
+++ init.pp
@@ -2,39 +2,37 @@ Package {
    allow_virtual => false
 }
 
-
 node default {
         include git
 
         class { 'nginx': }
 
         case $operatingsystem {
-              'RedHat', 'CentOS': {     class { selinux: mode => 'permissive' }
+              'RedHat', 'CentOS': { class { selinux: mode => 'permissive' }
             }
         }
 
         include my_fw
 
-        vcsrepo { '/var/www/demo':
-          ensure   => latest,
-          provider => git,
-          source   => 'https://github.com/puppetlabs/exercise-webpage.git',
-          revision => 'master',
-          notify => Service['nginx'],
-        }
-
-        nginx::resource::vhost { '_':
-                www_root => '/var/www/demo',
-                listen_port => 8000,
-                require   => File['/var/www'],
+	    vcsrepo { '/var/www/demo':
+			ensure   => latest,
+			provider => git,
+			source   => 'https://github.com/puppetlabs/exercise-webpage.git',
+			revision => 'master',
+			notify => Service['nginx'],
         }
 
         file { '/var/www':
-                ensure  => directory,
-                recurse => true,
-                owner => "nginx",
-                group => "nginx",
-                mode => 0500,
-                require   => Vcsrepo['/var/www/demo'],
+			ensure  => directory,
+			recurse => true,
+			owner => "www-data",
+			group => "www-data",
+			mode => 0500,
+			require   => Vcsrepo['/var/www/demo'],
         }
+
+        nginx::resource::vhost { '_':
+			www_root => '/var/www/demo',
+			listen_port => 8000,
+        }        
 }
