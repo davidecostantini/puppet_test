b5aa092734b49a47f73536768507d80e53ed7222
Davide <davide.costantini@infomentum.co.uk>
2015-07-17 15:20:22 +0100

    jj

--- init.pp
+++ init.pp
@@ -4,37 +4,37 @@ Package {
 
 
 node default {
-	include git
+        include git
 
-	class { 'nginx': }
+        class { 'nginx': }
 
-	case $operatingsystem {
-	      'RedHat', 'CentOS': {	class { selinux: mode => 'permissive' }
-	    }
-	}
+        case $operatingsystem {
+              'RedHat', 'CentOS': {     class { selinux: mode => 'permissive' }
+            }
+        }
 
-	include my_fw
+        include my_fw
 
-	vcsrepo { '/var/www/demo':
-	  ensure   => latest,
-	  provider => git,
-	  source   => 'https://github.com/puppetlabs/exercise-webpage.git',
-	  revision => 'master',
-	  notify => Service['nginx'],
-	}
+        vcsrepo { '/var/www/demo':
+          ensure   => latest,
+          provider => git,
+          source   => 'https://github.com/puppetlabs/exercise-webpage.git',
+          revision => 'master',
+          notify => Service['nginx'],
+        }
 
-	nginx::resource::vhost { '_':
-		www_root => '/var/www/demo',
-		listen_port => 8000,
-		require	  => File['/var/www/demo'],
-	}
+        nginx::resource::vhost { '_':
+                www_root => '/var/www/demo',
+                listen_port => 8000,
+                require   => File['/var/www'],
+        }
 
-	file { '/var/www/demo':
-		ensure  => directory,
-		recurse => true,
-		owner => "nginx",
-		group => "nginx",
-		mode => 0700,
-		require	  => Vcsrepo['/var/www/demo'],
-	}
+        file { '/var/www':
+                ensure  => directory,
+                recurse => true,
+                owner => "nginx",
+                group => "nginx",
+                mode => 0700,
+                require   => Vcsrepo['/var/www/demo'],
+        }
 }
