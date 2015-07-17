17d8e779ce5c1be5a4532b81eb0633ac886b5250
Davide <davide.costantini@infomentum.co.uk>
2015-07-17 16:33:25 +0100

    jj

---
+++ src/firewall.pp
@@ -0,0 +1,62 @@
+class my_fw {
+    #include my_fw::flush
+
+    Firewall {
+        before  => Class['my_fw::post'],
+        require => Class['my_fw::pre'],
+    }
+    class { ['my_fw::pre', 'my_fw::post']: }
+    class { 'firewall': }
+
+    include my_fw::webserver
+}
+
+
+class my_fw::webserver {
+  firewall { '008 Allow inbound SSH':
+    port     => 22,
+    proto    => tcp,
+    action   => accept,
+  }
+  firewall { '009 Allow Nginx 8000':
+    port     => 8000,
+    proto    => tcp,
+    action   => accept,
+  }  
+}
+
+class my_fw::pre {
+  Firewall {
+    require => undef,
+  }
+
+  # Default firewall rules
+  firewall { '000 accept all icmp':
+    proto   => 'icmp',
+    action  => 'accept',
+  }->
+  firewall { '001 accept all to lo interface':
+    proto   => 'all',
+    iniface => 'lo',
+    action  => 'accept',
+  }->
+  firewall { '002 accept related established rules':
+    proto   => 'all',
+    state => ['RELATED', 'ESTABLISHED'],
+    action  => 'accept',
+  }
+
+  #-----------------------------------
+}
+class my_fw::post {
+  firewallchain { 'INPUT:filter:IPv4':
+    ensure => present,
+    policy => drop,
+    before => undef,
+  }
+  firewallchain { 'FORWARD:filter:IPv4':
+    ensure => present,
+    policy => drop,
+    before => undef,
+  }
+}
